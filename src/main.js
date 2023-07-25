9e7011661ea59229afb6cc83e5c3da8d8edffd08
ernesthenry <henry38ernest@gmail.com>
2023-07-25 12:02:27 +0300

    Adding components

--- src/main.js
+++ src/main.js
@@ -2,10 +2,18 @@ import { createApp } from 'vue'
 import './style.css'
 import { createRouter,createWebHistory } from 'vue-router'  
 import App from './App.vue'
+import Home from '@views/Home.vue'
+import About from '@views/About.vue'
+
 
 const router =createRouter({
     history: createWebHistory(),
-    routes: []
+    routes: [
+        {path:'/', name: Home, component:Home},
+        {path:'/about', name: About, component:About}
+        // {path:'/', name: Home, component:Home}
+
+    ]
 })
 
 createApp(App)
