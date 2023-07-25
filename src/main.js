d099f814cb82a978c87de823c6ef2bc94cc38839
ernesthenry <henry38ernest@gmail.com>
2023-07-25 12:47:18 +0300

    Adding routes Refactor the codebase

--- src/main.js
+++ src/main.js
@@ -2,19 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import { createRouter,createWebHistory } from 'vue-router'  
 import App from './App.vue'
-import Home from '@views/Home.vue'
-import About from '@views/About.vue'
-
-
-const router =createRouter({
-    history: createWebHistory(),
-    routes: [
-        {path:'/', name: Home, component:Home},
-        {path:'/about', name: About, component:About}
-        // {path:'/', name: Home, component:Home}
-
-    ]
-})
+import router from '@/router'
 
 createApp(App)
 .use(router)
